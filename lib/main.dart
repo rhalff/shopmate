bc8695bf0c370a15fb66f44a5f5588c3d545fb8b
Rob Halff <rob.halff@gmail.com>
2019-08-08 23:43:16 +0200

    upgrade to new blocs api

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shopmate/app.dart';
 import 'package:shopmate/application.dart';
 
 import 'blocs/app_bloc_delegate.dart';
+import 'features/auth/blocs/blocs.dart';
+import 'features/cart/blocs/blocs.dart';
+import 'features/customer/blocs/blocs.dart';
 
 void main() async {
   final application = Application();
@@ -17,8 +21,21 @@ void main() async {
   await application.init();
 
   return runApp(
-    ShopMateApp(
-      application: application,
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>.value(
+          value: application.authBloc,
+        ),
+        BlocProvider<CartBloc>.value(
+          value: application.cartBloc,
+        ),
+        BlocProvider<CustomerBloc>.value(
+          value: application.customerBloc,
+        ),
+      ],
+      child: ShopMateApp(
+        application: application,
+      ),
     ),
   );
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -2,13 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'application.dart';
-import 'features/auth/blocs/blocs.dart';
-import 'features/cart/blocs/blocs.dart';
 import 'features/cart/screens/screens.dart';
 import 'features/catalog/blocs/blocs.dart';
 import 'features/catalog/screens/screens.dart';
 import 'features/checkout/screens/screens.dart';
-import 'features/customer/blocs/blocs.dart';
 import 'features/customer/screens/screens.dart';
 import 'features/department/screens/screens.dart';
 import 'home.dart';
@@ -22,133 +19,89 @@ RouteFactory routes({
     final Map<String, dynamic> arguments = settings.arguments;
     switch (settings.name) {
       case Home.route:
-        screen = Builder(builder: (BuildContext context) {
-          return BlocProviderTree(
-            blocProviders: [
-              BlocProvider<AuthBloc>(bloc: application.authBloc),
-              BlocProvider<CartBloc>(bloc: application.cartBloc),
-              BlocProvider<CustomerBloc>(bloc: application.customerBloc),
-              BlocProvider<DepartmentsBloc>(bloc: application.departmentsBloc),
-            ],
-            child: Home(),
-          );
-        });
+        screen = MultiBlocProvider(
+          providers: [
+            BlocProvider<DepartmentsBloc>.value(
+              value: application.departmentsBloc,
+            ),
+          ],
+          child: Home(),
+        );
         break;
       case DepartmentScreen.route:
-        screen = Builder(builder: (BuildContext context) {
-          return BlocProviderTree(
-            blocProviders: [
-              BlocProvider<AuthBloc>(bloc: application.authBloc),
-              BlocProvider<CartBloc>(bloc: application.cartBloc),
-              BlocProvider<CategoriesBloc>(bloc: application.categoriesBloc),
-              BlocProvider<CustomerBloc>(bloc: application.customerBloc),
-              BlocProvider<DepartmentsBloc>(bloc: application.departmentsBloc),
-              BlocProvider<FavoritesBloc>(bloc: application.favoritesBloc),
-              BlocProvider<ProductBloc>(bloc: application.productBloc),
-              BlocProvider<ProductsBloc>(bloc: application.productsBloc),
-            ],
-            child: DepartmentScreen(department: arguments['department']),
-          );
-        });
+        screen = MultiBlocProvider(
+          providers: [
+            BlocProvider<CategoriesBloc>.value(
+              value: application.categoriesBloc,
+            ),
+            BlocProvider<DepartmentsBloc>.value(
+              value: application.departmentsBloc,
+            ),
+            BlocProvider<FavoritesBloc>.value(
+              value: application.favoritesBloc,
+            ),
+            BlocProvider<ProductBloc>.value(
+              value: application.productBloc,
+            ),
+            BlocProvider<ProductsBloc>.value(
+              value: application.productsBloc,
+            ),
+          ],
+          child: DepartmentScreen(department: arguments['department']),
+        );
         break;
       case CatalogScreen.route:
-        screen = Builder(builder: (BuildContext context) {
-          return BlocProviderTree(
-            blocProviders: [
-              BlocProvider<AuthBloc>(bloc: application.authBloc),
-              BlocProvider<CartBloc>(bloc: application.cartBloc),
-              BlocProvider<CustomerBloc>(bloc: application.customerBloc),
-              BlocProvider<DepartmentsBloc>(bloc: application.departmentsBloc),
-              BlocProvider<FavoritesBloc>(bloc: application.favoritesBloc),
-              BlocProvider<ProductBloc>(bloc: application.productBloc),
-              BlocProvider<ProductsBloc>(bloc: application.productsBloc),
-            ],
-            child: CatalogScreen(),
-          );
-        });
+        screen = MultiBlocProvider(
+          providers: [
+            BlocProvider<DepartmentsBloc>.value(
+              value: application.departmentsBloc,
+            ),
+            BlocProvider<FavoritesBloc>.value(
+              value: application.favoritesBloc,
+            ),
+            BlocProvider<ProductBloc>.value(
+              value: application.productBloc,
+            ),
+            BlocProvider<ProductsBloc>.value(
+              value: application.productsBloc,
+            ),
+          ],
+          child: CatalogScreen(),
+        );
         break;
       case CartScreen.route:
-        screen = Builder(builder: (BuildContext context) {
-          return BlocProviderTree(
-            blocProviders: [
-              BlocProvider<AuthBloc>(bloc: application.authBloc),
-              BlocProvider<CartBloc>(bloc: application.cartBloc),
-              BlocProvider<CustomerBloc>(bloc: application.customerBloc),
-            ],
-            child: CartScreen(),
-          );
-        });
+        screen = CartScreen();
         break;
       case CheckoutScreen.route:
-        screen = Builder(builder: (BuildContext context) {
-          return BlocProviderTree(
-            blocProviders: [
-              BlocProvider<AuthBloc>(bloc: application.authBloc),
-              BlocProvider<CartBloc>(bloc: application.cartBloc),
-              BlocProvider<CustomerBloc>(bloc: application.customerBloc),
-            ],
-            child: CheckoutScreen(),
-          );
-        });
+        screen = CheckoutScreen();
         break;
       case ProductScreen.route:
-        screen = Builder(builder: (BuildContext context) {
-          return BlocProviderTree(
-            blocProviders: [
-              BlocProvider<AuthBloc>(bloc: application.authBloc),
-              BlocProvider<CartBloc>(bloc: application.cartBloc),
-              BlocProvider<CustomerBloc>(bloc: application.customerBloc),
-              BlocProvider<FavoritesBloc>(bloc: application.favoritesBloc),
-              BlocProvider<ProductBloc>(bloc: application.productBloc),
-              BlocProvider<ReviewsBloc>(bloc: application.reviewsBloc),
-            ],
-            child: ProductScreen(product: arguments['product']),
-          );
-        });
+        screen = MultiBlocProvider(
+          providers: [
+            BlocProvider<FavoritesBloc>.value(
+              value: application.favoritesBloc,
+            ),
+            BlocProvider<ProductBloc>.value(
+              value: application.productBloc,
+            ),
+            BlocProvider<ReviewsBloc>.value(
+              value: application.reviewsBloc,
+            ),
+          ],
+          child: ProductScreen(product: arguments['product']),
+        );
         break;
       case SignUpScreen.route:
-        screen = Builder(builder: (BuildContext context) {
-          return BlocProviderTree(
-            blocProviders: [
-              BlocProvider<AuthBloc>(bloc: application.authBloc),
-              BlocProvider<CustomerBloc>(bloc: application.customerBloc),
-            ],
-            child: SignUpScreen(),
-          );
-        });
+        screen = SignUpScreen();
         break;
       case SignInScreen.route:
-        screen = Builder(builder: (BuildContext context) {
-          return BlocProviderTree(
-            blocProviders: [
-              BlocProvider<AuthBloc>(bloc: application.authBloc),
-              BlocProvider<CustomerBloc>(bloc: application.customerBloc),
-            ],
-            child: SignInScreen(),
-          );
-        });
+        screen = SignInScreen();
         break;
       case ProfileScreen.route:
-        screen = Builder(builder: (BuildContext context) {
-          return BlocProviderTree(
-            blocProviders: [
-              BlocProvider<AuthBloc>(bloc: application.authBloc),
-              BlocProvider<CustomerBloc>(bloc: application.customerBloc),
-            ],
-            child: ProfileScreen(),
-          );
-        });
+        screen = ProfileScreen();
         break;
       case ForgotPasswordScreen.route:
-        screen = Builder(builder: (BuildContext context) {
-          return BlocProviderTree(
-            blocProviders: [
-              BlocProvider<AuthBloc>(bloc: application.authBloc),
-              BlocProvider<CustomerBloc>(bloc: application.customerBloc),
-            ],
-            child: ForgotPasswordScreen(),
-          );
-        });
+        screen = ForgotPasswordScreen();
         break;
       default:
         screen = ErrorContainer(
@@ -156,16 +109,6 @@ RouteFactory routes({
         );
     }
 
-    return MaterialPageRoute(
-      builder: (BuildContext context) => BlocBuilder(
-        bloc: application.authBloc,
-        builder: (
-          BuildContext context,
-          AuthState state,
-        ) {
-          return screen;
-        },
-      ),
-    );
+    return MaterialPageRoute(builder: (BuildContext context) => screen);
   };
 }
